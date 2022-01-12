35510afaa1707b93ea12f65f9def17edcba7822b
fhtozetto <fhtozetto@gmail.com>
2022-01-12 20:23:37 -0300

    working with queryRaw

---
+++ src/queryRaw/queryRaw.ts
@@ -0,0 +1,15 @@
+import { PrismaClient, Prisma, Courses } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const result = await prisma.$queryRaw<Courses[]>(
+        Prisma.sql`SELECT * FROM courses`
+    )
+
+    console.log(result);
+    result.map((item) => console.log(item.name));
+    
+}
+
+main();
